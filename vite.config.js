b78795eb537f041c6822355002bf3085ed4457d1
Andrei Kondratov <a.kondratov@slurm.io>
2024-08-04 15:09:43 +0500

    eslint fixes

--- vite.config.js
+++ vite.config.js
@@ -1,5 +1,5 @@
-import { defineConfig } from 'vite';
-import handlebars from 'vite-plugin-handlebars';
+import { defineConfig } from "vite";
+import handlebars from "vite-plugin-handlebars";
 
 export default defineConfig({
   plugins: [handlebars()],
