06eb74b17a3575316a4edbc6f66782a562d2e394
Andrei Kondratov <a.kondratov@slurm.io>
2024-08-19 20:51:53 +0500

    feat: store and light withStore hoc

--- src/core/EventBus.ts
+++ src/core/EventBus.ts
@@ -24,9 +24,7 @@ export default class EventBus {
       throw new Error(`Нет события: ${event}`);
     }
 
-    this.listeners[event] = this.listeners[event].filter(
-      (listener) => listener !== callback,
-    );
+    this.listeners[event] = this.listeners[event].filter((listener) => listener !== callback);
   }
 
   emit(event: string, ...args: unknown[]) {
